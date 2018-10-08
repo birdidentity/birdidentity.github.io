21c5eeb186561601b50dc60aef5b11373a8269b0
Vladislav Boyko <bird.identity@gmail.com>
2018-10-08 12:42:32 +0300

    finished with js pagination

--- github/src/js/script.js
+++ github/src/js/script.js
@@ -15,3 +15,23 @@ arrShow.forEach( function(item, i , arr) {
      item.classList.toggle('show');
    });
 });
+
+const paginationBtns = document.querySelectorAll('.pagination__button');
+
+paginationBtns.forEach( function(item, i , arr) {
+  item.addEventListener('click', function() {
+     if(item.classList.contains('active')) {
+
+     }else {
+       for(var i = 0; i < arr.length; i++) {
+         arr[i].classList.remove('active');
+       }
+       item.classList.add('active');
+       if(item === arr[1]) {
+         arr[0].classList.add('disabled');
+       }else {
+         arr[0].classList.remove('disabled');
+       }
+     }
+   });
+});
